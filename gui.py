8206b96059897ff25870bc1ddf959aed4b6e71f4
danidemi <escher75@gmail.com>
2015-06-13 16:56:22 +0200

    small update to gui

--- gui.py
+++ gui.py
@@ -4,6 +4,39 @@ class Application(Frame):
     def say_hi(self):
         print("hi there, everyone!")
 
+    def createWidgets2(self):
+
+        self.Display = Entry(self)
+        self.Display.pack({"side": "left"})
+
+        Operators = Frame(self)
+        self.PlusBtn = Button(Operators)
+        self.PlusBtn["text"] = "+"
+        self.PlusBtn["command"] = None
+        self.PlusBtn.pack({"side": "left"})
+        self.MinusBtn = Button(Operators)
+        self.MinusBtn["text"] = "-"
+        self.MinusBtn["command"] = None
+        self.MinusBtn.pack({"side": "left"})
+        self.EqualsBtn = Button(Operators)
+        self.EqualsBtn["text"] = "="
+        self.EqualsBtn["command"] = None
+        self.EqualsBtn.pack({"side": "left"})
+        Operators.pack()
+
+        Digits = Frame(self)
+        self.DigitBtn = []
+        for i in range(0,10):
+            TheButton = Button(Digits)
+            TheButton["text"] = i
+            TheButton["command"] = None
+            TheButton.pack({"side": "left"})
+            self.DigitBtn = self.DigitBtn + [TheButton]
+        Digits.pack()
+
+
+
+
     def createWidgets(self):
         self.QUIT = Button(self)
         self.QUIT["text"] = "QUIT"
@@ -21,7 +54,7 @@ class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
-        self.createWidgets()
+        self.createWidgets2()
 
 root = Tk()
 app = Application(master=root)
